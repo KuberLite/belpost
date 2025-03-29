d694f741b8020e5caa75ab29b4390cc5b1b89cd9
KuberLite <kuberlite@gmail.com>
2025-03-29 18:17:50 +0300

    Initial commit: create belpost gem structure

---
+++ spec/belpost_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Belpost do
+  it "has a version number" do
+    expect(Belpost::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
